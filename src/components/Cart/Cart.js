ba995d50216f2b72878f6ac7f903077c1c273a11
arfan36 <arfanur36@gmail.com>
2022-10-08 13:58:27 +0600

    54-4 Explore Conditional Rendering & conditional css class

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({ cart, handleRemoveItem }) => {
+
+    let message;
+    if (cart.length === 0) {
+        message = <p>Please buy at least one item !!!</p>;
+    }
+    else if (cart.length === 1) {
+        message = <div>
+            <h3>Amar jonno ekta nao</h3>
+            <p>tomar nijer jonno ekta</p>
+            <p><small>amar jonno arekta nao, please</small></p>
+        </div>;
+    }
+    else {
+        message = <p>Thanks for buying</p>;
+    }
+
     return (
         <div>
-            <h3>Order Summary</h3>
-            <h5>Order quantity: {cart.length}</h5>
+            <h3 className={cart.length === 2 ? `orange` : 'purple'}>Order Summary</h3>
+            <h5 className={`bold ${cart.length === 2 ? 'blue' : 'yellow'}`}>Order quantity: {cart.length}</h5>
             {
                 cart.map(tshirt => <p
                     key={tshirt._id}
@@ -13,8 +30,24 @@ const Cart = ({ cart, handleRemoveItem }) => {
                     <button onClick={() => handleRemoveItem(tshirt)}>X</button>
                 </p>)
             }
+            {
+                message
+            }
+            {cart.length === 3 ? <p>Tin jon ke gift diba?</p> : <p>kono kino</p>}
+            <p>and operator</p>
+            {cart.length === 2 && <h2>Double items</h2>}
+            <p>Or Operator</p>
+            {cart.length === 4 || <h2>Charta item na</h2>}
         </div>
     );
 };
 
 export default Cart;
+
+/* 
+//@ ───────────────────────────────────────────────── CONDITIONAL RENDERING ─────
+1. use element in a variable and then use if-else to set value
+2. ternary operation condition ? true : false
+3. && operator (if condition is true, i want to display something)
+4. || operator (if condition is false, i want to display something)
+ */
