edcff267a2ddc776a926c02b4f33d3c18d36e139
arfan36 <arfanur36@gmail.com>
2022-10-08 09:29:44 +0600

    54-2 Display TShirts and style t-shirt components

---
+++ src/components/TShirt/TShirt.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TShirt.css';
+
+const TShirt = ({ tshirt }) => {
+    const { _id, index, price, picture, name, gender } = tshirt;
+    return (
+        <div className='t-shirt'>
+            <img src={picture} />
+            <h2>{name}</h2>
+            <h4>Price: {price}</h4>
+            <button>Buy This</button>
+        </div>
+    );
+};
+
+export default TShirt;
